dcef61fc4af4f706331914c820311773e3495f66
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2022-10-19 18:44:32 +0300

    Insert function is done

--- list.c
+++ list.c
@@ -9,3 +9,27 @@ typedef struct Node {
 struct List {
     Node *head;
 };
+
+int insert(List *list, int value, int position) {
+    Node *currentNode = list->head;
+    while (currentNode->position != position) {
+        if (currentNode->next == NULL) {
+            return -1;
+        }
+        currentNode = currentNode->next;
+    }
+
+    Node *newNode = malloc(sizeof(Node));
+
+    newNode->value = value;
+    newNode->position = currentNode->position;
+    Node *nextNode = currentNode->next;
+    newNode->next = nextNode;
+
+    currentNode->next = newNode;
+    while (currentNode != NULL) {
+        ++currentNode->position;
+        currentNode = currentNode->next;
+    }
+    return 0;
+}
